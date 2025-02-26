0bdded3e942eae72fc7720c880055eebbbcc6cbe
ccitjan <ccitmarch2024@gmail.com>
2025-02-26 18:44:40 +0530

    Update Cloudinfra.tf

--- Cloudinfra.tf
+++ Cloudinfra.tf
@@ -7,15 +7,3 @@ resource "aws_s3_bucket" "ccit_s3" {
   bucket = "ccit-public-bucket"
   tags = { Name = "ccitbucket1" }
 }
-
-# Create an S3 bucket
-resource "aws_s3_bucket" "ccit_s3_2" {
-  bucket = "ccit-public-bucket2"
-  tags = { Name = "ccitbucket2" }
-}
-
-# Create an S3 bucket
-resource "aws_s3_bucket" "ccit_s3_3" {
-  bucket = "ccit-public-bucket3"
-  tags = { Name = "ccitbucket3" }
-}
