4f62ace23207fe6c1c1f82fb94ef4c5472d8f02c
ccitjan <ccitmarch2024@gmail.com>
2025-02-27 20:15:00 +0530

    Update Cloudinfra.tf

--- Cloudinfra.tf
+++ Cloudinfra.tf
@@ -2,19 +2,7 @@ provider "aws" {
   region = "ap-south-2"  # Updated AWS region
 }
 # Create an S3 bucket
-/*resource "aws_s3_bucket" "ccit_s3" {
+resource "aws_s3_bucket" "ccit_s3" {
   bucket = "ccit-public-bucket1"
   tags = {"Environment": "dev"}
-}*/
-
-resource "aws_instance" "firstinstance" {
-  ami           = "ami-0a19d948cf5ce40b2"
-  instance_type = "t3.micro"
-
-  tags = {
-   "Name"          = "EC2-CCIT",
-    "Environment"   = "Dev",
-    "Owner"         = "CCIT"
-
-  }
 }
