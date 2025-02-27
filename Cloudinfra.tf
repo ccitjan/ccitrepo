fa9cc23a171898eabe2cd4371894165cef3afb7f
ccitjan <ccitmarch2024@gmail.com>
2025-02-27 20:47:45 +0530

    Update Cloudinfra.tf

--- Cloudinfra.tf
+++ Cloudinfra.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 # Create an S3 bucket
 resource "aws_s3_bucket" "ccit_s3" {
-  bucket = "ccit-public-bucket1"
+  bucket = "ccit-public-bucket0"
   tags = {"Environment": "dev"}
 }
